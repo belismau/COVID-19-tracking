a7358a6301c690247fbad88531ba9e38a7108043
Bel <belismau@gmail.com>
2020-04-24 14:40:19 +0200

    Added border-bottom effect on input

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 import theme from './js/Theme.js';
 
 theme()
+
+document.getElementsByTagName('input')[0].addEventListener('focusin', function() {
+    document.getElementById('borderBottomEffect').style.width = '100%';
+    document.getElementsByTagName('button')[0].style.color = '#727272';
+});
+
+document.getElementsByTagName('input')[0].addEventListener('focusout', function() {
+    document.getElementById('borderBottomEffect').style.width = '0';
+    document.getElementsByTagName('button')[0].style.color = '#9b9b9b';
+});
