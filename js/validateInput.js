403355d055568f25a5ff0795cc680058e4c203d5
Bel <belismau@gmail.com>
2020-04-27 22:33:09 +0200

    Added code to showCountryInfo() + improved the interface

--- js/validateInput.js
+++ js/validateInput.js
@@ -23,13 +23,23 @@ function showCountryInfo(name) {
         dataType: 'JSON'
     }).done(function(data) {
         $('<section id="countryInfo"></section>').insertAfter('#inputContainer')
-        for (let i = (data.length - 1); i >= 0; i--) {
+        for (let i = (data.length - 1); i >= 14; i--) {
             let date = (data[i].Date).slice(0, 10)
             $('#countryInfo').append('<h1>' + date + '</h1>')
-            $('#countryInfo').append('<p> Tot Confirmed: ' + data[i].Confirmed + '</p>')
-            $('#countryInfo').append('<p> Tot Deaths: ' + data[i].Deaths + '</p>')
-            $('#countryInfo').append('<p> Tot Recovered: ' + data[i].Recovered + '</p>')
-            $('#countryInfo').append('<p> Tot Aactive: ' + data[i].Active + '</p>')
+
+            $('#countryInfo').append('<div class="table"></div>')
+
+            $('.table').append('<div><p>SE</p></div>')
+            $('.table').append('<div><p> Confirmed </p></div>')
+            $('.table').append('<div><p> Deaths </p></div>')
+            $('.table').append('<div><p> Recovered </p></div>')
+            $('.table').append('<div><p> Active </p></div>')
+
+            $('.table').append('<div><p> Total </p></div>')
+            $('.table').append('<div><p>' + data[i].Confirmed + '</p></div>')
+            $('.table').append('<div><p>' + data[i].Deaths + '</p></div>')
+            $('.table').append('<div><p>' + data[i].Recovered + '</p></div>')
+            $('.table').append('<div><p>' + data[i].Active + '</p></div>')
 
             if (i != 0) {
                 let newConfirmed = (data[i].Confirmed) - (data[i - 1].Confirmed)
@@ -37,10 +47,11 @@ function showCountryInfo(name) {
                 let newRecovered = (data[i].Recovered) - (data[i - 1].Recovered)
                 let newActive = (data[i].Active) - (data[i - 1].Active)
 
-                $('#countryInfo').append('<p> New Confirmed: ' + newConfirmed + '</p>')
-                $('#countryInfo').append('<p> New Deaths: ' + newDeaths + '</p>')
-                $('#countryInfo').append('<p> New Recovered: ' + newRecovered + '</p>')
-                $('#countryInfo').append('<p> New Active: ' + newActive + '</p>')
+                $('.table').append('<div><p> New </p></div>')
+                $('.table').append('<div><p>' + newConfirmed + '</p></div>')
+                $('.table').append('<div><p>' + newDeaths + '</p></div>')
+                $('.table').append('<div><p>' + newRecovered + '</p></div>')
+                $('.table').append('<div><p>' + newActive + '</p></div>')
             }
         }
         $('html, body').animate({
