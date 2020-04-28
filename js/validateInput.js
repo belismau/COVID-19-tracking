57b49a62e20c9a50a61fb5c07f0479c4b0ff8231
Bel <belismau@gmail.com>
2020-04-28 13:15:10 +0200

    Improved the functionality of presenting country information

--- js/validateInput.js
+++ js/validateInput.js
@@ -23,36 +23,17 @@ function showCountryInfo(name) {
         dataType: 'JSON'
     }).done(function(data) {
         $('<section id="countryInfo"></section>').insertAfter('#inputContainer')
-        for (let i = (data.length - 1); i >= 14; i--) {
+        let classNr = 1
+        for (let i = (data.length - 1); i >= 0; i--) {
             let date = (data[i].Date).slice(0, 10)
             $('#countryInfo').append('<h1>' + date + '</h1>')
-
             $('#countryInfo').append('<div class="table"></div>')
-
-            $('.table').append('<div><p>SE</p></div>')
-            $('.table').append('<div><p> Confirmed </p></div>')
-            $('.table').append('<div><p> Deaths </p></div>')
-            $('.table').append('<div><p> Recovered </p></div>')
-            $('.table').append('<div><p> Active </p></div>')
-
-            $('.table').append('<div><p> Total </p></div>')
-            $('.table').append('<div><p>' + data[i].Confirmed + '</p></div>')
-            $('.table').append('<div><p>' + data[i].Deaths + '</p></div>')
-            $('.table').append('<div><p>' + data[i].Recovered + '</p></div>')
-            $('.table').append('<div><p>' + data[i].Active + '</p></div>')
-
+            addDescriptionRow(classNr)
+            addTotalRow(classNr, data, i)
             if (i != 0) {
-                let newConfirmed = (data[i].Confirmed) - (data[i - 1].Confirmed)
-                let newDeaths = (data[i].Deaths) - (data[i - 1].Deaths)
-                let newRecovered = (data[i].Recovered) - (data[i - 1].Recovered)
-                let newActive = (data[i].Active) - (data[i - 1].Active)
-
-                $('.table').append('<div><p> New </p></div>')
-                $('.table').append('<div><p>' + newConfirmed + '</p></div>')
-                $('.table').append('<div><p>' + newDeaths + '</p></div>')
-                $('.table').append('<div><p>' + newRecovered + '</p></div>')
-                $('.table').append('<div><p>' + newActive + '</p></div>')
+                addNewRow(classNr, data, i)
             }
+        classNr += 1
         }
         $('html, body').animate({
             scrollTop: $("#countryInfo").offset().top
@@ -62,8 +43,38 @@ function showCountryInfo(name) {
     });
 };
 
+function addDescriptionRow(classNr) {
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>Sweden</p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p> Confirmed </p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p> Deaths </p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p> Recovered </p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p> Active </p></div>')
+}
+
+function addTotalRow(classNr, data, i) {
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p> Total </p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>' + data[i].Confirmed + '</p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>' + data[i].Deaths + '</p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>' + data[i].Recovered + '</p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>' + data[i].Active + '</p></div>')
+}
+
+function addNewRow(classNr, data, i) {
+    let newConfirmed = (data[i].Confirmed) - (data[i - 1].Confirmed)
+    let newDeaths = (data[i].Deaths) - (data[i - 1].Deaths)
+    let newRecovered = (data[i].Recovered) - (data[i - 1].Recovered)
+    let newActive = (data[i].Active) - (data[i - 1].Active)
+
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p> New </p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>' + newConfirmed + '</p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>' + newDeaths + '</p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>' + newRecovered + '</p></div>')
+    $('.table:nth-of-type(' + classNr + ')').append('<div><p>' + newActive + '</p></div>')
+}
+
 function showError() {
     console.log('error')
 };
+
 
 export default validateInput
